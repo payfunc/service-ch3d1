3e543fcdb5bd25102cbf261c810772b1129d3992
Robin Nedergård <robin.nedergard@payfunc.com>
2020-07-22 14:14:52 +0200

    changed external structure

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,40 @@
-import { Configuration } from "./Configuration"
-import * as check from "./check"
-import * as enrolled from "./enrolled"
+import { Card as apiCard } from "./Card"
+import { check as apiCheck } from "./check"
+import { enrolled as apiEnrolled } from "./enrolled"
+import { Configuration as apiConfiguration } from "./Configuration"
 
-export {
-	Configuration,
-	check,
-	enrolled,
+export type Configuration = apiConfiguration
+export namespace Configuration {
+	export const is = apiConfiguration.is
+}
+export type Card = apiCard
+export namespace Card {
+	export const is = apiCard.is
+	export const from = apiCard.from
+	export const to = apiCard.to
+}
+// tslint:disable-next-line: no-shadowed-variable
+export namespace check {
+	export const post = apiCheck.post
+	export type Request = apiCheck.Request
+	export namespace Request {
+		export const is = apiCheck.Request.is
+	}
+	export type Response = apiCheck.Response
+	export namespace Response {
+		export const is = apiCheck.Response.is
+		export const verify = apiCheck.Response.verify
+	}
+}
+// tslint:disable-next-line: no-shadowed-variable
+export namespace enrolled {
+	export const post = apiEnrolled.post
+	export type Request = apiEnrolled.Request
+	export namespace Request {
+		export const is = apiEnrolled.Request.is
+	}
+	export type Response = apiEnrolled.Response
+	export namespace Response {
+		export const is = apiEnrolled.Response.is
+	}
 }
--- index.spec.ts
+++ index.spec.ts
@@ -1,13 +1,13 @@
-import * as service from "./index"
+import * as ch3d1 from "./index"
 
 describe("service-ch3d1", () => {
 	it("test accessibility", () => {
-		service.ch3d1.Eci.is({})
-		const pareqFunctions = [service.ch3d1.Pareq.decode, service.ch3d1.Pareq.encode, service.ch3d1.Pareq.is]
-		const paresFunctions = [service.ch3d1.Pares.decode, service.ch3d1.Pares.encode, service.ch3d1.Pares.is]
-		const cardFunctions = [service.ch3d1.api.Card.from, service.ch3d1.api.Card.to, service.ch3d1.api.Card.is]
-		service.ch3d1.api.Configuration.is({})
-		const checkFunctions = [service.ch3d1.api.check.Request.is, service.ch3d1.api.check.Response.is, service.ch3d1.api.check.Response.verify, service.ch3d1.api.check.post]
-		const enrolledFunctions = [service.ch3d1.api.enrolled.Request.is, service.ch3d1.api.enrolled.Response.is, service.ch3d1.api.enrolled.post]
+		ch3d1.Eci.is({})
+		const pareqFunctions = [ch3d1.Pareq.decode, ch3d1.Pareq.encode, ch3d1.Pareq.is]
+		const paresFunctions = [ch3d1.Pares.decode, ch3d1.Pares.encode, ch3d1.Pares.is]
+		const cardFunctions = [ch3d1.api.Card.from, ch3d1.api.Card.to, ch3d1.api.Card.is]
+		ch3d1.api.Configuration.is({})
+		const checkFunctions = [ch3d1.api.check.Request.is, ch3d1.api.check.Response.is, ch3d1.api.check.Response.verify, ch3d1.api.check.post]
+		const enrolledFunctions = [ch3d1.api.enrolled.Request.is, ch3d1.api.enrolled.Response.is, ch3d1.api.enrolled.post]
 	})
 })
