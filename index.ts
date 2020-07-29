78d1d83c3cf9457b75498d96d9404c84b9071ae4
Robin Nedergård <robin.nedergard@payfunc.com>
2020-07-29 13:35:58 +0200

    empty request parameter removed

--- index.ts
+++ index.ts
@@ -3,8 +3,8 @@ import * as gracely from "gracely"
 import * as model from "@payfunc/model"
 import * as api from "./api"
 
-async function check(key: authly.Token, merchant: model.Merchant.Key, request: api.check.Request, token: authly.Token): Promise<api.check.Response | gracely.Error> {
-	return (!merchant.card || merchant.card.emv3d?.protocol != "ch3d1") ? gracely.client.unauthorized() : api.check.post({ url: merchant.card.url, key }, request, token)
+async function check(key: authly.Token, merchant: model.Merchant.Key, token: authly.Token): Promise<api.check.Response | gracely.Error> {
+	return (!merchant.card || merchant.card.emv3d?.protocol != "ch3d1") ? gracely.client.unauthorized() : api.check.post({ url: merchant.card.url, key }, {}, token)
 }
 
 async function enrolled(key: authly.Token, merchant: model.Merchant.Key, request: api.enrolled.Request, token: authly.Token): Promise<api.enrolled.Response | gracely.Error> {
