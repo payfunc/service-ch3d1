7acd0133b0f8e713bd3c799bb31641392ae86dd3
Robin Nedergård <robin.nedergard@payfunc.com>
2020-08-05 15:23:15 +0200

    missed export

--- index.spec.ts
+++ index.spec.ts
@@ -4,6 +4,6 @@ describe("service-ch3d1", () => {
 	it("test accessibility", () => {
 		ch3d1.api.Eci.is({})
 		const checkFunctions = [ch3d1.check, ch3d1.api.check.Request.is, ch3d1.api.check.Response.verify, ch3d1.api.check.Response.is, ch3d1.api.check.Error.is]
-		const enrolledFunctions = [ch3d1.enrolled, ch3d1.api.enrolled.Request.orderIdLimit, ch3d1.api.enrolled.Request.is, ch3d1.api.enrolled.Response.is]
+		const enrolledFunctions = [ch3d1.enrolled, ch3d1.api.enrolled.Request.orderIdLimit, ch3d1.api.enrolled.Request.is, ch3d1.api.enrolled.Response.is, ch3d1.api.enrolled.Response.isOk, ch3d1.api.enrolled.Response.error]
 	})
 })
